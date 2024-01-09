4889a9c729b0bc38edae58cf6b307f70bc0715fc
Nzyazin <69486560+Nzyazin@users.noreply.github.com>
2024-01-09 18:13:24 +0300

    changing file migration Products

--- database/migrations/2024_01_08_121705_create_products_table.php
+++ database/migrations/2024_01_08_121705_create_products_table.php
@@ -12,21 +12,20 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
+            $table->id();
             $table->string('name');
-            $table->integer('product_id')->primary();
+            $table->string('product_id');
             $table->string('url');
-            $table->integer('price');
-            $table->integer('old_price');
+            $table->string('price');
+            $table->string('old_price');
             $table->string('currencyId');            
             $table->string('picture');            
             $table->string('vendor');
-            $table->unsignedBigInteger('category');                        
-            $table->unsignedBigInteger('sub_category');
-            $table->unsignedBigInteger('sub_sub_category');
+
+            $table->string('category')->nullable();
+            $table->string('sub_category')->nullable();
+            $table->string('sub_sub_category')->nullable();            
             
-            $table->foreign('category')->references('category_id')->on('categories')->onDelete('cascade')->onUpdate('cascade');                        
-            $table->foreign('sub_category')->references('sub_category_id')->on('subcategories')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreign('sub_sub_category')->references('sub_sub_category_id')->on('subsubcategories')->onDelete('cascade')->onUpdate('cascade');
             $table->boolean('available');
             
             $table->timestamps();

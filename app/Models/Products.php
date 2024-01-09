7706a16d6f5d57de1ac8738a60a962d1b764634c
Nzyazin <69486560+Nzyazin@users.noreply.github.com>
2024-01-09 18:16:34 +0300

    adding method createWithCategories

--- app/Models/Products.php
+++ app/Models/Products.php
@@ -7,22 +7,40 @@ use Illuminate\Database\Eloquent\Model;
 class Products extends Model
 {
     protected $table = 'products';
+    protected $fillable = ['name', 'product_id', 'url', 'price', 
+    'old_price', 'currencyId', 'picture', 'vendor',
+    'category', 'sub_category', 'sub_sub_category', 'available'
+    ];
 
-    // Отношение многие к одному с категориями
-    public function category()
-    {
-        return $this->belongsTo(Categories::class, 'category');
-    }
+    
 
-    // Отношение многие к одному с подкатегориями
-    public function subcategory()
+    public static function createWithCategories($shopArray)
     {
-        return $this->belongsTo(Subcategories::class, 'sub_category');
-    }
+        foreach ($shopArray['offers'] as $offer) {
+            $product = new self();
+            $subsubcategory = Subsubcategories::find((integer) $offer['category']);
+            if ($subsubcategory) {
+                $product->sub_sub_category = $subsubcategory->name;
+                $product->sub_category = $subsubcategory->parent_name;
+                $subcategory = Subcategories::find($subsubcategory->parent_id);
+                $product->category = $subcategory->parent_name;
+            } else {
+                $subcategory = Subcategories::find((integer) $offer['category']);
+                $product->sub_category = $subcategory->name;
+                $product->category = $subcategory->parent_name;
+            }         
 
-    // Отношение многие к одному с подподкатегориями
-    public function subsubcategories()
-    {
-        return $this->belongsTo(Subsubcategories::class, 'sub_sub_category');;
+            $product->name = $offer['name'];
+            $product->product_id = $offer['product_id'];
+            $product->url = $offer['url'];
+            $product->price = $offer['price'];
+            $product->old_price = $offer['old_price'];
+            $product->currencyId = $offer['currencyId'];
+            $product->picture = $offer['picture'];
+            $product->vendor = $offer['vendor'];
+            $product->available = $offer['available'];
+            $product->save();
+        }        
+        return $product;
     }
 }

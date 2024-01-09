c530c98e9d33448dc338eaa5af7fe0b988604503
Nzyazin <69486560+Nzyazin@users.noreply.github.com>
2024-01-09 18:16:59 +0300

    Update PageController.php

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,81 +2,82 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Http;
+use App\Models\Categories;
+use App\Models\Subcategories;
+use App\Models\Subsubcategories;
+use App\Models\Products;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Services\XmlProcessingService;
 
 class PageController extends Controller
 {
+    private $xmlService;
+
+    public function __construct(XmlProcessingService $xmlService)
+    {
+        $this->xmlService = $xmlService;
+    }
+    
     public function showForm() 
     {
         return view('upload-xml');
     }
 
     public function downloadAndProcessXml(Request $request)
-    {
+    {   
+        $validator = Validator::make($request->all(), [
+            'xmlFile' => 'required|file|mimes:xml|max:2048', // Проверка наличия файла, типа, и размера
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        $xmlFile = $request->file('xmlFile');
+
+        // Сохраняем файл во временный каталог
+        $path = $xmlFile->storeAs('temp', 'temp.xml');
+
+        // Путь к сохраненному файлу
+        $filePath = storage_path('app/' . $path);
+
+        $shopArray = $this->xmlService->parseXmlToArray($filePath); 
+
+        //Цикл для записи категорий в БД
+        foreach ($shopArray['categories'] as $key => $value) {
+            $categories = new Categories([
+                'category_id' => $key,
+                'name' => $value['name']
+            ]);
+            $categories->save();
+            
+            //Цикл для записи подкатегорий в БД
+            foreach ($shopArray['categories'][$key]['subcategory'] as $key2 => $value2) {
+                $subcategories = new Subcategories([
+                    'sub_category_id' => $key2,
+                    'name' => $value2['name'],
+                    'parent_id' => $key,
+                    'parent_name' => $value['name']
+                ]);
+                $subcategories->save();
+
+                //Цикл для записи подподкатегорий в БД
+                foreach ($shopArray['categories'][$key]['subcategory'][$key2]['subsubcategory'] as $key3 => $value3) {                    
+                    $subsubcategories = new Subsubcategories([
+                        'sub_sub_category_id' => $key3,
+                        'name' => $value3['name'],
+                        'parent_id' => $key2,
+                        'parent_name' => $value2['name']
+                    ]);
+                    $subsubcategories->save();
+                }
+            }
+        }
+        //dd($shopArray);
+        $product = Products::createWithCategories($shopArray);
         
-
-
         
-        $url = 'https://quarta-hunt.ru/bitrix/catalog_export/export_Ngq.xml';
-
-        //Используем Guzzle для выполнения HTTP-запроса
-        $response = Http::get($url);
-        
-        // Проверка успешности запроса
-        if ($response->successful()) {
-            $xmlContent = $response->body();
-            $xmlArray = simplexml_load_string($xmlContent);
-            $categoriesArray = [];
-            foreach ($xmlArray->shop->categories->category as $category) {
-
-                $categoryId = (string) $category['id'];
-                $categoryName = (string) $category;
-
-                $parentId = isset($category['parentId']) ? (string) $category['parentId'] : null;   
-                if ($parentId == null) {
-                    $categoriesArray[$categoryId] = [
-                        'id' => $categoryId,
-                        'name' => $categoryName,
-                        'subcategory' => [],
-                    ];
-                    
-                    foreach ($xmlArray->shop->categories->category as $subcategory) {
-                        $subcategoryId = (string) $subcategory['id'];
-                        $subcategoryName = (string) $subcategory;
-                        $parentId2 = isset($subcategory['parentId']) ? (string) $subcategory['parentId'] : null; 
-
-                        if ($categoryId == $parentId2) {
-                            $categoriesArray[$categoryId]['subcategory'][$subcategoryId] = [
-                                'id' => $subcategoryId,
-                                'name' => $subcategoryName,
-                                'parent_id' => $parentId2,
-                                'subsubcategory' => [],
-                            ];                            
-
-                            foreach ($xmlArray->shop->categories->category as $subsubcategory) {
-                                $subsubcategoryId = (string) $subsubcategory['id'];
-                                $subsubcategoryName = (string)$subsubcategory;
-                                $parentId3 = isset($subsubcategory['parentId']) ? (string) $subsubcategory['parentId'] : null;
-                                //dd($parentId3);
-                                if ($subcategoryId == $parentId3) {
-                                    //dd($productId);
-
-                                    $categoriesArray[$categoryId]['subcategory'][$subcategoryId]['subsubcategory'][$subsubcategoryId] = [
-                                        'id' => $subsubcategoryId,
-                                        'name' => $subsubcategoryName,
-                                        'parent_id' => $parentId3,
-                                    ];
-                                }
-                            }
-                            
-                        }                        
-                    } 
-                }                                        
-            }          
-        } else {
-            throw new \Exception('Ошибка получения XML');
-        }    
-        return view('upload-xml');
+        return view('upload-xml', $product);
     }
 }
